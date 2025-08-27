812ee9f788397e560064488a5b63a099a58a32d9
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-08-27 23:35:36 +0300

    add task 2 in the course

--- 2/script.js
+++ 2/script.js
@@ -1,16 +1,61 @@
-//courseName,courseDuration, courseOwner
+const replace = {
+  [Symbol.replace](words = "JavaScript", replacer = "...") {
+    if (words.length >= 15) {
+      return `${words.slice(0, 15)}${replacer}`;
+    }
+    return words;
+  },
+};
 
-function task1(options = {}) {
-  const ALLOWEDPROP = ["courseName", "courseDuration", "courseOwner"];
+function task1() {
+  console.log("hello".replace(replace));
+  console.log("hello, world everyone".replace(replace));
+}
 
-  if (!ALLOWEDPROP.every((key) => Object.keys(options).includes(key))) {
+const myObj = {
+  myName: "Marwan",
+  age: 19,
+  [Symbol.iterator]() {
+    const keys = Object.entries(this);
+    let index = 0;
+    return {
+      next() {
+        if (keys.length > index) {
+          return {
+            value: keys[index++],
+            done: false,
+          };
+        } else {
+          return {
+            done: true,
+          };
+        }
+      },
+    };
+  },
+};
+
+function task2() {
+  for (const [key, value] of myObj) {
+    console.log(`key:${key}, value: ${value}`);
+  }
+}
+
+function task3(options) {
+  const ALLOWED_PROPERTIES = ["courseName", "courseDuration", "courseOwner"];
+  const keys = Object.keys(options);
+  if (
+    ALLOWED_PROPERTIES.length !== keys.length ||
+    !ALLOWED_PROPERTIES.every((key) => keys.includes(key))
+  ) {
     throw new Error(
-      `can not work with this object: ${Object.entries(options)}`
+      `can not work with this object: ${Object.entries(options)}`,
     );
   }
-
   return `courseName is: ${options.courseName}, course duration is: ${options.courseDuration} and course owner is: ${options.courseOwner}`;
 }
 
-console.log(task1({ courseName: "", courseDuration: "", courseOwner: "" }));
-// task1({ hello: "world" });
+// task1();
+// task2();
+// console.log(task3({ courseName: "", courseDuration: "", courseOwner: "" }));
+// task3({});
